1abf462eb3b79fcb8d4ba00d1d07df1317a5ec80
zhangwen <13802881532@139.com>
2013-12-25 10:37:06 +0800

    增加PluginDEMO代码 增加cordova lib文件及引用 增加cordova config文件，配置plugin 增加assets\www下web页面DEMO 增加eclipce项目描述 增加android manifest权限 修改android minsdk version至8

--- src/cn/wxcs/smartapp/DemoPlugin.java
+++ src/cn/wxcs/smartapp/DemoPlugin.java
@@ -1,5 +1,33 @@
 package cn.wxcs.smartapp;
 
-public class DemoPlugin {
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
 
+import android.content.Context;
+import android.os.Vibrator;
+import android.util.Log;
+
+public class DemoPlugin extends CordovaPlugin {
+	private static final String TAG = DemoPlugin.class.getSimpleName();
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    	Log.d(TAG, "Action: " + action);
+    	Log.d(TAG, "Arguments: " + args.toString());
+    	this.echo(args.getString(0), callbackContext);
+        return false;
+    }
+
+    private void echo(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+        	Log.d(TAG, "begin vibrate...");
+        	Vibrator vibrator = (Vibrator) this.cordova.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+            vibrator.vibrate(1000);
+            callbackContext.success(message);
+        } else {
+        	Log.d(TAG, "No Messages,return error");
+            callbackContext.error("参数不能为空");
+        }
+    }
 }
--- src/cn/wxcs/smartapp/MyActivity.java
+++ src/cn/wxcs/smartapp/MyActivity.java
@@ -1,9 +1,11 @@
 package cn.wxcs.smartapp;
 
+import org.apache.cordova.CordovaActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
+public class MyActivity extends CordovaActivity  {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +13,8 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        super.loadUrl("file:///android_asset/www/index.html");
+        super.appView.getSettings().setUserAgentString(appView.getSettings().getUserAgentString() + " | SmartAPP 0.1 pre alpha");
     }
 }
